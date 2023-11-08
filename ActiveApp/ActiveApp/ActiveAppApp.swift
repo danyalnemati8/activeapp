01d8324f45f47f32a522f75013404e654c647218
Danyal Nemati <Dnemati@csu.fullerton.edu>
2023-11-07 16:48:00 -0800

    first commit

---
+++ ActiveApp/ActiveApp/ActiveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActiveAppApp.swift
+//  ActiveApp
+//
+//  Created by Danyal Nemati on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ActiveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActiveApp/ActiveApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ActiveApp
+//
+//  Created by Danyal Nemati on 11/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
